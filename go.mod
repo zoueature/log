ff63fddb811a40f51edb7efc6b4352bbb3073e2a
crazyman <me@crazyman.com>
2023-12-04 11:36:41 +0800

    update log implement

--- go.mod
+++ go.mod
@@ -4,12 +4,13 @@ go 1.21.4
 
 require (
 	github.com/google/uuid v1.4.0
-	github.com/sirupsen/logrus v1.9.3
-	gitlab.jiebu.com/base/config v0.0.1
+	gitlab.jiebu.com/base/config v0.0.3
+	go.uber.org/zap v1.26.0
 )
 
 require (
 	github.com/spf13/cast v1.6.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- log.go
+++ log.go
@@ -3,27 +3,131 @@ package log
 import (
 	"context"
 	"github.com/google/uuid"
-	"github.com/sirupsen/logrus"
 	"gitlab.jiebu.com/base/config"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"os"
+	"path/filepath"
 )
 
-var logger = logrus.New()
+var zapLogger *zap.Logger
 
-const logIdKey = "log-id"
+const (
+	logIdKey  = "log-id"
+	userIdKey = "user-id"
+	uriKey    = "uriKey"
 
-func Init(cfg *config.LogConfig) error {
-	logrus.SetFormatter(&logrus.JSONFormatter{})
-	writer, err := cfg.GetLogWriter()
+	defaultLogPath = "./logs"
+)
+
+func init() {
+	var err error
+	zapLogger, err = zap.NewProduction()
 	if err != nil {
-		return err
+		panic(err)
 	}
-	logrus.SetOutput(writer)
-	logger = logrus.StandardLogger()
+}
+
+type apiCtx interface {
+	AuthUserID() int
+	RequestURI() string
+}
+
+type debugLevelEnabler string
+
+func (e debugLevelEnabler) Enabled(level zapcore.Level) bool {
+	return level <= zapcore.DebugLevel
+}
+
+type infoLevelEnabler string
+
+func (e infoLevelEnabler) Enabled(level zapcore.Level) bool {
+	return level > zapcore.DebugLevel && level < zapcore.ErrorLevel
+}
+
+type errorLevelEnabler string
+
+func (e errorLevelEnabler) Enabled(level zapcore.Level) bool {
+	return level >= zapcore.ErrorLevel
+}
+
+// Configure 配置log
+func Configure(cfg *config.Configuration) error {
+	if cfg.App.Debug {
+		var err error
+		zapLogger, err = zap.NewDevelopment()
+		if err != nil {
+			return err
+		}
+	} else {
+		debugWs, err := getLogWriter(*cfg.Log, "debug")
+		if err != nil {
+			return err
+		}
+		infoWs, err := getLogWriter(*cfg.Log, "info")
+		if err != nil {
+			return err
+		}
+		errorWs, err := getLogWriter(*cfg.Log, "error")
+		if err != nil {
+			return err
+		}
+		prodconf := zap.NewProductionEncoderConfig()
+		// 实例化日志器
+		debugCore := zapcore.NewCore(zapcore.NewJSONEncoder(prodconf), debugWs, debugLevelEnabler(""))
+		infoCore := zapcore.NewCore(zapcore.NewJSONEncoder(prodconf), infoWs, infoLevelEnabler(""))
+		errCore := zapcore.NewCore(zapcore.NewJSONEncoder(prodconf), errorWs, errorLevelEnabler(""))
+
+		zapLogger = zap.New(zapcore.NewTee(debugCore, infoCore, errCore))
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 type setter interface {
 	Set(key string, value interface{})
+}
+
+// getLogWriter 获取日志输出方式  日志文件 控制台
+func getLogWriter(conf config.LogConfig, level string) (zapcore.WriteSyncer, error) {
+	if conf.Driver == config.StdoutDriver {
+		// 日志同时输出到控制台
+		return zapcore.AddSync(os.Stdout), nil
+	}
+
+	// 判断日志路径是否存在，如果不存在就创建
+	if exist := IsExist(conf.Path); !exist {
+		if conf.Path == "" {
+			conf.Path = defaultLogPath
+		}
+		if err := os.MkdirAll(conf.Path, os.ModePerm); err != nil {
+			// 指定目录失败， 则写在默认目录
+			conf.Path = defaultLogPath
+			if err := os.MkdirAll(conf.Path, os.ModePerm); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	// 日志文件 与 日志切割 配置
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   filepath.Join(conf.Path, level+".log"), // 日志文件路径
+		MaxSize:    conf.LogFileMaxSize,                    // 单个日志文件最大多少 mb
+		MaxBackups: conf.LogFileMaxBackups,                 // 日志备份数量
+		MaxAge:     conf.LogMaxAge,                         // 日志最长保留时间
+		Compress:   true,                                   // 是否压缩日志
+	}
+	// 日志只输出到日志文件
+	return zapcore.AddSync(lumberJackLogger), nil
+}
+
+// IsExist 判断文件或者目录是否存在
+func IsExist(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
 }
 
 // InjectLogID 注入logid到上下文中
@@ -32,52 +136,37 @@ func InjectLogID(ctx setter) {
 	ctx.Set(logIdKey, logIdVal)
 }
 
-// Debug 输出Debug日志
-func Debug(ctx context.Context, args ...interface{}) {
-	logger.WithField(logIdKey, ctx.Value(logIdKey)).Debug(args...)
+func injectCommonValue(ctx context.Context) *zap.Logger {
+	ac, ok := ctx.(apiCtx)
+
+	logger := zapLogger.With(zap.Any(logIdKey, ctx.Value(logIdKey)))
+	if ok {
+		logger = logger.With(zap.Any(userIdKey, ac.AuthUserID()), zap.Any(uriKey, ac.RequestURI()))
+	}
+	return logger
 }
 
-// Debugf 输出Debug日志
-func Debugf(ctx context.Context, format string, args ...interface{}) {
-	logger.WithField(logIdKey, ctx.Value(logIdKey)).Debugf(format, args...)
+// Debug 输出Debug日志
+func Debug(ctx context.Context, msg string, fields ...zap.Field) {
+	injectCommonValue(ctx).Debug(msg, fields...)
 }
 
 // Info 输出Info日志
-func Info(ctx context.Context, args ...interface{}) {
-	logger.WithField(logIdKey, ctx.Value(logIdKey)).Info(args...)
-}
-
-// Infof 输出Info日志
-func Infof(ctx context.Context, format string, args ...interface{}) {
-	logger.WithField(logIdKey, ctx.Value(logIdKey)).Infof(format, args...)
+func Info(ctx context.Context, msg string, fields ...zap.Field) {
+	injectCommonValue(ctx).Info(msg, fields...)
 }
 
 // Warn 输出Warn日志
-func Warn(ctx context.Context, args ...interface{}) {
-	logger.WithField(logIdKey, ctx.Value(logIdKey)).Warn(args...)
-}
-
-// Warnf 输出Warn日志
-func Warnf(ctx context.Context, format string, args ...interface{}) {
-	logger.WithField(logIdKey, ctx.Value(logIdKey)).Warnf(format, args...)
+func Warn(ctx context.Context, msg string, fields ...zap.Field) {
+	injectCommonValue(ctx).Warn(msg, fields...)
 }
 
 // Error 输出error日志
-func Error(ctx context.Context, args ...interface{}) {
-	logger.WithField(logIdKey, ctx.Value(logIdKey)).Error(args...)
-}
-
-// Errorf 输出error日志
-func Errorf(ctx context.Context, format string, args ...interface{}) {
-	logger.WithField(logIdKey, ctx.Value(logIdKey)).Errorf(format, args...)
+func Error(ctx context.Context, msg string, fields ...zap.Field) {
+	injectCommonValue(ctx).Error(msg, fields...)
 }
 
 // Panic 输出Panic日志
-func Panic(ctx context.Context, args ...interface{}) {
-	logger.WithField(logIdKey, ctx.Value(logIdKey)).Panic(args...)
-}
-
-// Panicf 输出Panic日志
-func Panicf(ctx context.Context, format string, args ...interface{}) {
-	logger.WithField(logIdKey, ctx.Value(logIdKey)).Panicf(format, args...)
+func Panic(ctx context.Context, msg string, fields ...zap.Field) {
+	injectCommonValue(ctx).Panic(msg, fields...)
 }
